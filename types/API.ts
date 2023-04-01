fc7f7307ce5d99f56f26408251b28e28738d4ced
DEVRafam <jodax95@gmail.com>
2023-04-01 15:00:09 +0200

    PAWEL MASZ WIECEJ DO ROBIENIA

--- types/API.ts
+++ types/API.ts
@@ -9,3 +9,20 @@ export interface Task {
 export interface User {
     //
 }
+
+// ! PAWEL !!!
+// Zro jakis request co zwroci dane w tym interfejsie:
+export interface StatsAPIRequest {
+    monthly: Statistics
+    annualy: Statistics
+    general: Statistics
+}
+
+export interface Statistics {
+    /** Miejsce na ktorym jest osoba */
+    place: number
+    points: number
+    avatar: string
+    firstName: string
+    lastName: string
+}
